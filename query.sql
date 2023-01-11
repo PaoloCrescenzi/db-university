9431f69e418b6072ac86efb36835fe34bdcdf381
PaoloCrescenzi <crescenzipaolo2@gmail.com>
2023-01-11 16:24:40 +0100

    fine

--- query.sql
+++ query.sql
@@ -31,5 +31,10 @@
     WHERE `level` = "magistrale";
     
 --7. Da quanti dipartimenti è composta l'università? (12)
+    SELECT COUNT(`id`)
+    FROM `departments`;
 
 --8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
+    SELECT * 
+    FROM `teachers`
+    WHERE `phone` IS NULL;
