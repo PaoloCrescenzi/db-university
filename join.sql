c8b3d7e495a1e093230fd7becfa93896e50b5bd9
PaoloCrescenzi <crescenzipaolo2@gmail.com>
2023-01-12 19:13:39 +0100

    query 2

--- join.sql
+++ join.sql
@@ -6,7 +6,12 @@
     WHERE `degrees`.`name` = "Corso di Laurea in Economia";
 
 --2. Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di Neuroscienze
-
+    SELECT `degrees` .*, `departments` .name
+    FROM `degrees`
+    INNER JOIN `departments`
+    ON `degrees` .department_id = `departments` .id
+    WHERE `departments` .name = "Dipartimento di Neuroscienze"
+    AND`degrees` .level = "magistrale";
 
 --3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
 
